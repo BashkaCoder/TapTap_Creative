2b05fb5ec5a49d8e67caf23af06974dc66a86518
Danylo <danworkbashinsky@gmail.com>
2025-08-08 15:14:21 +0300

    tralalelo

--- Assets/_Project/Scripts/RifleShooter.cs
+++ Assets/_Project/Scripts/RifleShooter.cs
@@ -3,10 +3,22 @@ using UnityEngine;
 public class RifleShooter : MonoBehaviour
 {
     [SerializeField] private Rifle _rifle;
-    
+    [SerializeField] private Animator _animator;
+
+    private static readonly int ShootingParam = Animator.StringToHash("Shooting");
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) _rifle.StartShooting();
-        if (Input.GetMouseButtonUp(0)) _rifle.StopShooting();
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rifle.StartShooting();
+            _animator.SetBool(ShootingParam, true); // запуск анимации
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _rifle.StopShooting();
+            _animator.SetBool(ShootingParam, false); // возврат к Sitting
+        }
     }
 }
